420136f591764dc4d8ba223f13debf64ab88c952
boris <boristondjua@gmail.com>
2026-03-30 16:49:29 +0200

    done with fixture

--- tests/test_regression/test_case_3_fixture.py
+++ tests/test_regression/test_case_3_fixture.py
@@ -1,4 +1,3 @@
-
 # Test Case 3: Login User with incorrect email and password
 from playwright.sync_api import Page, expect, Playwright
 
@@ -12,17 +11,10 @@ def test_login_User_with_incorrect_email_and_password(go_to_page_login, fake_cre
 
 
 # Firefox
-def test_login_User_with_incorrect_email_and_password_firefox(playwright: Playwright):
-    firefoxBrowser = playwright.firefox.launch(headless=False)
-    page = firefoxBrowser.new_page()
-    page.goto("https://www.automationexercise.com/")
-    expect(page.get_by_text("Video Tutorials")).to_be_visible()
-    page.get_by_role("button", name="consent").click()
-    page.get_by_role("link", name="Signup / Login").click()
-    expect(page.get_by_text("Login to your account")).to_be_visible()
-    page.locator('[data-qa="login-email"]').fill("flase@gmail.com")
-    page.locator('[data-qa="login-password"]').fill("Freedom95")
+def test_login_User_with_incorrect_email_and_password_firefox(test_login_User_firefox_login, fake_credentials):
+    page = test_login_User_firefox_login
+    page.locator('[data-qa="login-email"]').fill(fake_credentials["email"])
+    page.locator('[data-qa="login-password"]').fill(fake_credentials["password"])
     page.locator('[data-qa="login-button"]').click()
     expect(page.get_by_text("Your email or password is incorrect!")).to_be_visible()
-    time.sleep(4)
-    firefoxBrowser.close()
+
